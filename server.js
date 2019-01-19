409a30d0a5733122b6f939de0a5bf33311a2f8a0
Kirby <legendofkirby85@gmail.com>
2019-01-18 17:38:25 -0800

    touch up server

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
-var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 var logger = require('morgan'); 
-var request = require('request'); 
-var cheerio = require('cheerio');
+// var request = require('request'); 
+// var cheerio = require('cheerio');
 
 var app = express();
 app.use(logger('dev'));
@@ -34,8 +33,8 @@ db.once('open', function() {
   console.log('Mongoose connection successful.');
 });
 
-var Comment = require('./models/Note.js');
-var Article = require('./models/Article.js');
+// var Comment = require('./models/Note.js');
+// var Article = require('./models/Article.js');
 
 var router = require('./controllers/controller.js');
 app.use('/', router);
